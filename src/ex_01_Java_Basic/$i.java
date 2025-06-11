9977a64c0cab30f0776ac229814970a322db5d9c
jakkaaditya <jakkaaditya@gmail.com>
2025-06-11 19:30:09 +0530

    Arithmetic_operators

--- src/ex_01_Java_Basic/$i.java
+++ src/ex_01_Java_Basic/$i.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class testing {
+public class $i {
     public static void main(String[] args)
     {
         System.out.println("Hello test");
---
+++ src/ex_04_Operators/Operators_Arithmetic_operators.java
@@ -0,0 +1,19 @@
+package ex_04_Operators;
+
+public class Operators_Arithmetic_operators {
+
+    public static void main (String[] args)
+    {  int age = 30;
+        int age2 = 65;
+
+        // int -> data type
+        // age -> identifier
+        // = -> Assignment operator
+        // 65 -> Literal
+        System.out.println(age +age2 );
+        System.out.println(age-age2);
+        System.out.println(age*age2);
+        System.out.println(age/age2);
+
+    }
+}
